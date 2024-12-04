00f005c204d6fd39af669f15bc21a8692e445c58
evansril <161180656+evansril@users.noreply.github.com>
2024-12-04 14:25:33 +0000

    Initial commit

---
+++ footer.php
@@ -0,0 +1,7 @@
+	  <hr>
+
+      <footer>
+        <div class="foot_center"><p>GROUP 2 &copy; ELECTORIAL COMMISSION GHANA</p>
+		<p>&nbsp;&nbsp;&nbsp;&nbsp;ENSURING FREE AND FAIR ELECTION</p>
+		</div>
+      </footer>
---
+++ footer1.php
@@ -0,0 +1,7 @@
+	  <hr>
+
+      <footer>
+        <div class="foot_center2"><p>GROUP 2 &copy;ELECTORIAL COMMISION GHNA</p>
+		<p>&nbsp;&nbsp;&nbsp;&nbsp;ENSURING FREE AND FAIR ELECTION</p>
+		</div>
+      </footer>
